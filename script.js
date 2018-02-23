7d71f12f7f70fe534005a0d9bc0a645de9750894
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-02-23 16:25:24 +0700

    Add rotate cam

--- script.js
+++ script.js
@@ -2,57 +2,41 @@ var txt1;
 var txt2;
 var txt3;
 
-var camX = 1;
-var camY = 201;
-var camZ = 1;
+var easycam;
 
-var camXX = true;
-var camYY = false;
-var camZZ = true;
+var pos = true;
+
+setTimeout(function(){
+	       pos = false;
+	   },60000);
 
 function setup(){
     createCanvas(windowWidth,  windowHeight, WEBGL);
+  
+    console.log(Dw);
+    console.log(Dw.EasyCam.INFO);
+    
+    easycam = createEasyCam();
+
     txt3 = loadImage("assets/bg.jpg");
     txt1 = loadImage("assets/txt1.jpg");
     txt2 = loadImage("assets/txt2.jpg");
 }
 
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    easycam.setViewport([0,0,windowWidth, windowHeight]);
+}
+
 function draw(){
     background(0);
-
-    if(camX >= 200) {
-	camXX = false
-    } else if(camY >= 200) {
-	camYY = false
-    } else if (camZ >= 200) {
-	camZZ = false
-    } else if(camX <= 0) {
-	camXX = true
-    } else if(camY <= 0) {
-	camYY = true
-    } else if(camZ <=0) {
-	camZZ = true
-    }
-
-    if(camXX){
-	camX+=2;
+    
+    if(pos){
+	easycam.rotateY(0.005);	
     } else {
-	camX-=2;
+	easycam.rotateX(0.005);
     }
-
-    if(camYY){
-	camY+=2;
-    } else {
-	camY-=2;
-    }
-
-    if(camZZ){
-	camZ+=2;
-    } else {
-	camZ-=2;
-    }
-
-    camera(camX, camY, 0, 0, 0, 0, camX*2, camY*2, 0);
+    
     
     var locY = (mouseY / height - 0.5) * (-2);
     var locX = (mouseX / width - 0.5) * 2;
@@ -65,15 +49,13 @@ function draw(){
     push();
     translate(-250, 0, 0);
     rotateY(frameCount * 0.05);
-    rotateZ(frameCount * 0.05);
     ambientMaterial(250);
     texture(txt1);
     sphere(90, 64);
     pop();
     
     translate(250, 0, 0);
-    rotateZ(frameCount * 0.05);
-    rotateX(frameCount * 0.05);
+    rotateY(frameCount * 0.05);
     ambientMaterial(250);
     texture(txt2);
     sphere(120, 64);
